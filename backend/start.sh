cd4c64c23fe7b3b8a4b2f7a4d69f6fd8a4036bc8
serenedev6 <serenenight3335@gmail.com>
2026-04-20 22:30:07 -0500

    Add gunicorn to requirements.txt

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,4 @@
 python setup_sample_db.py
 python manage.py migrate
 nginx
-gunicorn config.wsgi:application --bind 127.0.0.1:8000 --workers 2
+/usr/local/bin/gunicorn config.wsgi:application --bind 127.0.0.1:8000 --workers 2
