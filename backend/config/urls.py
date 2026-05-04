407b1c9292709f7ab2edfcf8c4f94af396dfbae3
serenedev6 <serenenight3335@gmail.com>
2026-05-03 21:57:30 -0500

    Added load balancer health endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health/',health_check)
     path('', include('chat.urls')),
 ]
