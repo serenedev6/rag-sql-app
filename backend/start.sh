6b107ca029c85a3d55d494fce4879ecf23f74692
serenedev6 <serenenight3335@gmail.com>
2026-05-05 14:47:22 -0500

    Removed Nginx from Dockerfile and start.sh files as we are now using ALB

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python setup_sample_db.py
 python manage.py migrate
-nginx
-/usr/local/bin/gunicorn config.wsgi:application --bind 127.0.0.1:8000 --workers 2
+/usr/local/bin/gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2
